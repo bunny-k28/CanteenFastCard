9d94294804ae9697b97213561f7e2262b4fc9101
Arman Das <58970641+bunny-k28@users.noreply.github.com>
2024-02-24 21:19:53 +0530

    Made scanner module dynamic for Win and Mac

--- CFC/scanner.py
+++ CFC/scanner.py
@@ -8,29 +8,65 @@ from pyzbar.pyzbar import decode
 # Specify the zbar library path
 os.environ['DYLD_LIBRARY_PATH'] = '/opt/homebrew/Cellar/zbar/0.23.93/lib/libzbar.dylib'
 
-def scan_barcode():
-    cap = cv2.VideoCapture(0)
-    data = ''
 
-    while True:
-        frame = cap.read()[-1]
-        barcodes = decode(frame)
+class ScannerForDarwin(object):
+    def __init__(self):
+        self.scanned_data = int
 
-        for barcode in barcodes:
-            data = barcode.data.decode('utf-8')
-            print("Barcode data:", data)
+    def __del__(self):
+        self.video.release()
 
-            pts = np.array([barcode.polygon], np.int32)
-            pts = pts.reshape((-1, 1, 2))
+    def get_frame(self):
+        self.video = cv2.VideoCapture(0)
+        frame = self.video.read()[-1]
 
-            cv2.polylines(frame, [pts], True, (0, 0, 255), 5)
+        while True:
+            frame = self.video.read()[-1]
+            barcodes = decode(frame)
 
-            return int(data)
-        
-        # Display the frame (flipped)
-        cv2.flip(frame, 1, frame)
-        cv2.imshow('Barcode Scanner', frame)
+            for barcode in barcodes:
+                data = barcode.data.decode('utf-8')
+                print("Barcode data:", data)
 
-        # Exit if data is digit and length is 7
-        if cv2.waitKey(1) == ord('q'):
-            break
+                if data.__len__ == 7: self.scanned_data = data
+
+
+                pts = np.array([barcode.polygon], np.int32)
+                pts = pts.reshape((-1, 1, 2))
+
+                cv2.polylines(frame, [pts], True, (0, 0, 255), 5)
+
+            cv2.flip(frame, 1, frame)
+            jpeg = cv2.imencode('.jpg', frame)[-1]
+
+            return jpeg.tobytes()
+
+
+class ScannerForWin32(object):
+    def scan(self):
+        cap = cv2.VideoCapture(0)
+        data = ''
+
+        while True:
+            frame = cap.read()[-1]
+            barcodes = decode(frame)
+
+            for barcode in barcodes:
+                data = barcode.data.decode('utf-8')
+                print("Barcode data:", data)
+
+                pts = np.array([barcode.polygon], np.int32)
+                pts = pts.reshape((-1, 1, 2))
+
+                cv2.polylines(frame, [pts], True, (0, 0, 255), 5)
+
+                if data.__len__() == 7: return int(data)
+                else: continue
+            
+            # Display the frame (flipped)
+            cv2.flip(frame, 1, frame)
+            cv2.imshow('Barcode Scanner', frame)
+
+            # Exit if data is digit and length is 7
+            if cv2.waitKey(1) == ord('q'):
+                break
